9294f08a88df5b28fab5fbe7930df692a8b4ce8b
Jose Luis DLS <jdelossantos1@ucol.mx>
2025-05-29 01:33:21 -0600

    script merge

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Variables de estado
 let currentFontSize = 0;
 let isDarkMode = false;
@@ -90,45 +89,3 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
-=======
-let fontSizeLevel = 0;
-
-function increaseFontSize() {
-  if (fontSizeLevel < 3) {
-    document.documentElement.style.fontSize = `${16 + fontSizeLevel * 4 + 4}px`;
-    fontSizeLevel++;
-  }
-}
-
-function decreaseFontSize() {
-  if (fontSizeLevel > -3) {
-    document.documentElement.style.fontSize = `${16 + fontSizeLevel * 4 - 4}px`;
-    fontSizeLevel--;
-  }
-}
-
-function toggleFont() {
-  document.body.style.fontFamily = 
-    document.body.style.fontFamily === "Arial" ? "Verdana" : "Arial";
-}
-
-function toggleDarkMode() {
-  document.body.classList.toggle("dark-mode");
-}
-
-function toggleContrast() {
-  document.body.classList.toggle("high-contrast");
-}
-
-function toggleCursor() {
-  document.body.classList.toggle("cursor-big");
-}
-
-function toggleImages() {
-  document.querySelectorAll("img").forEach(img => img.classList.toggle("hidden"));
-}
-
-function toggleLinks() {
-  document.querySelectorAll("a").forEach(link => link.classList.toggle("highlighted"));
-}
->>>>>>> 03357fefa2177b6443da43b8829132bc09642d63
